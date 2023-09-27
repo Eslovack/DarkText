d8f081b8dad0407bd72aa6e77160f19fb4104cbf
Cadu <carlos.esf@aluno.ifsc.edu.br>
2023-09-27 16:42:35 -0300

    Jogador Classe

---
+++ src/codigo/Jogador.java
@@ -0,0 +1,70 @@
+package codigo;
+
+public class Jogador {
+	private String nome;
+	private String classe;
+	private Float force;
+	private Float vida;
+	private Float vidaMax;
+	private Float manaMax;
+	private Float mana;
+	private Float velociadeMax;
+	private Float velocidade;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getClasse() {
+		return classe;
+	}
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	public Float getForce() {
+		return force;
+	}
+	public void setForce(Float force) {
+		this.force = force;
+	}
+	public Float getVida() {
+		return vida;
+	}
+	public void setVida(Float vida) {
+		this.vida = vida;
+	}
+	public Float getVidaMax() {
+		return vidaMax;
+	}
+	public void setVidaMax(Float vidaMax) {
+		this.vidaMax = vidaMax;
+	}
+	public Float getManaMax() {
+		return manaMax;
+	}
+	public void setManaMax(Float manaMax) {
+		this.manaMax = manaMax;
+	}
+	public Float getMana() {
+		return mana;
+	}
+	public void setMana(Float mana) {
+		this.mana = mana;
+	}
+	public Float getVelociadeMax() {
+		return velociadeMax;
+	}
+	public void setVelociadeMax(Float velociadeMax) {
+		this.velociadeMax = velociadeMax;
+	}
+	public Float getVelocidade() {
+		return velocidade;
+	}
+	public void setVelocidade(Float velocidade) {
+		this.velocidade = velocidade;
+	}
+	
+	
+
+}
