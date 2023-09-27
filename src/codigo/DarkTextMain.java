c05b2b2a664d5c3f1bcf559e111904db7cce6f6e
jesus <lucas.r2006@aluno.ifsc.edu.br>
2023-09-27 17:15:42 -0300

    modo criativo 1

--- src/codigo/DarkTextMain.java
+++ src/codigo/DarkTextMain.java
@@ -26,21 +26,30 @@ public class DarkTextMain {
 			System.out.println("Deseja utilizar o modo criativo \n 1-sim \n qualquer outra tecla pra não ");
 			op = Integer.valueOf(leitura.nextLine());
 			if (op == 1) {
-				
+			System.out.println("O que deseja fazer? \n 0-sair \n 1-alterar nome \n 2-listar os atributos dos inimigos \n 3-adicionar inimigo \n 4-remover área");
+			op = Integer.valueOf(leitura.nextLine());
 			switch (op) {
 			case 0: {
 				break;
 			}
 			case 1: {
-
+				System.out.println("Qual seu novo nome");
+				jogador.setNome(leitura.nextLine());
 				break;
 			}
 			case 2: {
 
 				break;
-			}}
 			}
-
+			case 3: {
+				break;
+			}
+			case 4: {
+				break;
+			}
+			}
+			}
+			System.out.println(jogador.getNome());
 		}
 
 		leitura.close();
